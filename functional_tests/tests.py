d1e82fb3cbf3c2ede452fe874ffd5e51052d2d9f
giddygamer505 <xvvsti@gmail.com>
2026-03-12 15:51:57 +0700

    change link to text and button

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -19,6 +19,7 @@ class visit_web(StaticLiveServerTestCase):
     def test_people_see_candidates(self):
         #people use the web-app "Election"
         self.browser.get(self.live_server_url)
+        self.assertIn('Election', self.browser.title)
 
         #They see a big "Election" text
         header = self.browser.find_element(By.TAG_NAME,'h1')
@@ -26,13 +27,13 @@ class visit_web(StaticLiveServerTestCase):
         
 
         #They see the first candidate name as a link (Jose Carlo)
-        first_candidate = self.browser.find_element(By.TAG_NAME,'a')
+        first_candidate = self.browser.find_element(By.TAG_NAME,'h3')
         self.assertEqual('Jose Carlo',first_candidate.text)
         time.sleep(1)
-
-        #Someone interest so he click Jose's link
-        first_candidate.click()
-        time.sleep(1)
+        
+        #He see button to vote
+        jose_btn = self.browser.find_element(By.ID,'first_btn')
+        self.assertEqual(jose_btn.text,"Vote to Jose")
 
 
         
