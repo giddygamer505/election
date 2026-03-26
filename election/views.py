e3a0a0cf941293803b7c123ce7074c276caa8e6f
giddygamer505 <xvvsti@gmail.com>
2026-03-26 23:04:03 +0700

    vote point

--- election/views.py
+++ election/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Candidate
 from django.db.models import F
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 def home(request):
     candidates = Candidate.objects.all().order_by('name')
     return render(request, 'home.html', {'candidates': candidates})
 
-def vote(request):
-    pass
+def vote(request,candidate_id):
+    if request.method == 'POST':
+        candidate = get_object_or_404(Candidate, pk=candidate_id)
+        Candidate.objects.filter(pk=candidate_id).update(votes=F('votes') + 1)
+        return redirect('election:home') 
+        
+    return redirect('election:home')
